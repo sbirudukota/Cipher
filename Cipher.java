121e5c4731489d00952e86f59ffbfdb571550432
sbirudukota <87544834+sbirudukota@users.noreply.github.com>
2021-07-18 17:28:32 +0530

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -10,7 +10,7 @@ public class Cipher{
 	   
       Random rand = new Random();   //create a random object for key
       int [] data = new int[5];    //scanning numbers using arrays and scanner
-      try (Scanner sc = new Scanner(System.in)) {
+      try (Scanner sc = new Scanner(System.in)) {    //create scanner object 
 	      
 		  int key = rand.nextInt(9) + 1;
 	      
@@ -19,7 +19,7 @@ public class Cipher{
 		  System.out.println("PROGRAMMING ASSIGNMENT 1");
 		  System.out.println("Welcome to the Cipher program");
 	      
-		  System.out.println("Please enetr 5 numbers beween 0 and 19"); // 
+		  System.out.println("Please enetr 5 numbers beween 0 and 19"); // user input 
 		  System.out.print("1st number: ");
 		  data[0] = sc.nextInt();
 	      
@@ -39,9 +39,9 @@ public class Cipher{
 		  for (int i = 0; i<5; i++){
 		      if (data[i] < 0 || data[i] > 19){
 		         System.out.println("Please read instructions and try again");
-		         System.exit(1);//terminate the program
+		         System.exit(1);     //terminate the program
 		      }
-		      sum = sum + data[i];//Sum & store numbers in a variable
+		      sum = sum + data[i];   //Sum & store numbers in a variable
 		  }
 	      
 		  System.out.println("Total = " + sum);
@@ -54,9 +54,11 @@ public class Cipher{
 		      System.out.println("Your encoded number is " + a);
 		  }
 		  else {
+			  
 		      //separating the numbers
 		      int a = sum / 10;
 		      int b = sum % 10;
+			  
 		      //encoding
 		      a = (key + a) % 10;
 		      b = (key + b) % 10;
